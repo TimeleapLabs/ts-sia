396e680930cb28b1afc8e3424c3b7057b907a26a
Hamedblue1381 <hamed.balanar@gmail.com>
2025-07-07 13:59:16 +0330

    refactor: update ascii method names in other files

--- src/benchmark/tests/sia.ts
+++ src/benchmark/tests/sia.ts
@@ -8,11 +8,11 @@ export const siaFiveThousandUsers = () =>
     .seek(0)
     .addArray16(fiveThousandUsers, (sia, user) => {
       sia
-        .addAscii(user.userId)
-        .addAscii(user.username)
-        .addAscii(user.email)
-        .addAscii(user.avatar)
-        .addAscii(user.password)
+        .addAscii8(user.userId)
+        .addAscii8(user.username)
+        .addAscii8(user.email)
+        .addAscii8(user.avatar)
+        .addAscii8(user.password)
         .addInt64(user.birthdate.valueOf())
         .addInt64(user.registeredAt.valueOf());
     })
@@ -22,11 +22,11 @@ const encoded = siaFiveThousandUsers();
 const desia = new Sia(encoded);
 
 const decodeUser = (sia: Sia) => ({
-  userId: sia.readAscii(),
-  username: sia.readAscii(),
-  email: sia.readAscii(),
-  avatar: sia.readAscii(),
-  password: sia.readAscii(),
+  userId: sia.readAscii8(),
+  username: sia.readAscii8(),
+  email: sia.readAscii8(),
+  avatar: sia.readAscii8(),
+  password: sia.readAscii8(),
   birthdate: new Date(sia.readInt64()),
   registeredAt: new Date(sia.readInt64()),
 });
